87bbdb2b397d413c54cf3e48bc58228b6488f797
Fazil vk <76457800+fazilvk786@users.noreply.github.com>
2021-11-02 11:57:47 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+require("dotenv").config();
+
+module.exports = {
+  session: JSON.parse(
+    process.env.SESSION ||
+      fs.readFileSync(__dirname + "/session.json", { encoding: "utf8" })
+  ), //if not using env vars create a file named session.json
+  };
