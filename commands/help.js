fe904d3fff3bb0d9409bac4bd66be3f4e9bc4d55
Fazil vk <76457800+Fazil-vk@users.noreply.github.com>
2022-01-18 19:10:09 +0530

    Elsa wa-bot v1 ✨

--- commands/help.js
+++ commands/help.js
@@ -41,7 +41,6 @@ const execute = async (client,msg,args) => {
     else {
         await client.sendMessage(msg.from, `No command with the name *${args[0]}*...`);
     }
-    client.sendMessage(msg.to, `*======Elsa Wa Bot======*\n\n\`\`\`\Join Our WhatsApp Support Group :\`\`\`\n *${link}${defaultXyz.j}* \n\n\`\`\`\Telegram Suppport Group: \n ${defaultXyz.t}${tele} \`\`\`\n\n*===========💖==========*`);
     
 };
 
