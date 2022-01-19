12d26ef097b1eded0181ea0b3db83b52688eaed3
Fazil vk <76457800+Fazil-vk@users.noreply.github.com>
2022-01-19 06:11:03 +0530

    Update ytmp3.js

--- commands/ytmp3.js
+++ commands/ytmp3.js
@@ -10,12 +10,12 @@ async function execute(client, msg, args) {
         url: url
     }
 
-    axios.request(options).then(async function (response) {
+    axios.request(options).then(function (response) {
         var data = response.data
     	var title = data.judul;
         var thumb = data.thumbnail;
         var link = data.link;
-        const media = await MessageMedia.fromUrl(thumb);
+        const media = MessageMedia.fromUrl(thumb);
         client.sendMessage(msg.from, media, {caption: 'here\'s my media'})
         msg.reply(`*${title}* \n\n*Thumb: *${thumb}\n\n*YT Link:* ${link}\n\n*© Elsa Wa-Bot*`);
         client.MessageMedia(msg.channel, link);
