d4aca47d4b5408559aae96cf4b03ed2d178cd183
Fazil vk <76457800+Fazil-vk@users.noreply.github.com>
2022-01-21 11:08:16 +0530

    Update textpro.js

--- commands/textpro.js
+++ commands/textpro.js
@@ -20,8 +20,9 @@ const execute = async (client, msg, args) => {
             .then(async function (data) { 
               try { 
                   console.log(data);
-                  const media = await MessageMedia.fromUrl(data);
+                  const media = await MessageMedia.fromUrl('https://textpro.me/images/user_image/2022/01/61ea44bad2e39.jpg');
                   client.sendMessage(msg.from, media, {caption: "*©️ Elsa Wa-Bot*"});
+                  break;
               } catch(err) { 
                   console.log(err)
                   msg.reply("```" + "Error occured" + "```");
