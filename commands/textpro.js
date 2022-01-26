457f542f50675c78baeb6c588bb39a1f5f4f58c4
Fazil vk <76457800+Fazil-vk@users.noreply.github.com>
2022-01-26 12:59:57 +0530

    Update textpro.js

--- commands/textpro.js
+++ commands/textpro.js
@@ -25,11 +25,11 @@ const execute = async (client, msg, args) => {
         if (splitted[0] == textpr[i].name) {
             thiccysapi.textpro(textpr[i].url, splitted[1])
             .then(async (data) => { 
+                const media = await MessageMedia.fromUrl(link);
+                client.sendMessage(msg.from, media, {caption: "*©️ Elsa Wa-Bot*"});
               try { 
                   console.log(data);
                   link = data
-                  const media = await MessageMedia.fromUrl(link);
-                  client.sendMessage(msg.from, media, {caption: "*©️ Elsa Wa-Bot*"});
               } catch(err) { 
                   console.error(err)
                   msg.reply("```" + "Error occured" + "```");
