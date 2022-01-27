40966d232af2491885a2c03ca13561d4b45db097
Fazil vk <76457800+Fazil-vk@users.noreply.github.com>
2022-01-27 22:43:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@
     puppeteer: { headless: true, args: ['--no-sandbox'], },
     //if you are using railway or other, then change remove this paths. Tis is only for heroku
     executablePath: "/root/whatsapp-bot/.apt/usr/bin/google-chrome",
-    ffmpegPath: "/root/whatsapp-bot/.apt/usr/bin/ffmpeg",
+    ffmpegPath: "ffmpeg",
   });
   
   client.commands = new Map();
